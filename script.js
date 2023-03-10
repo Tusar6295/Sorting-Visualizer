2354442667e5cee78594a2822ba4044cd1c2e5c1
Tusar6295 <tusar_202000219@smit.smu.edu.in>
2023-03-10 14:05:40 +0530

    new style added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const cont = document.querySelector('.container');
+const cont = document.querySelector('.bars_container');
 const newArray = document.querySelector('#random_array')
 const sort = document.querySelector('#sort')
 
@@ -32,7 +32,7 @@ function bars(array){
     {
         const bar = document.createElement('div')
         bar.classList.add('bar');
-        bar.style.height = array[i] * 10 + "px";
+        bar.style.height = array[i] * 15 + "px";
         cont.appendChild(bar);
     }
 }
@@ -43,36 +43,43 @@ sort.addEventListener('click' , function(){
 
 function sleep(ms)
 {
-    return new Promise((r) => setTimeout(r,ms));
+    return new Promise((resolve) => setTimeout(resolve,ms));
 }
+
 async function bubbleSort(arr)
 {
     const bars = document.getElementsByClassName('bar')
     for(let i = 0; i<arr.length; i++)
     {
-        for(let j = 0; j<arr.length - i; j++)
+        for(let j = 0; j<arr.length - i - 1; j++)
         {
+            bars[j].style.backgroundColor = "green"
+            bars[j+1].style.backgroundColor = "green"  
+            await sleep(1) 
             if(arr[j] > arr[j+1])
             {
                 for(let k=0; k<bars.length; k++)
                 {
                     if(k !==j && k !== j+1)
                     {
-                        bars[k].style.backgroundColor = "aqua";
+                        bars[k].style.backgroundColor = "white";
                     }
                 }
                 let temp;
                 temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
-                bars[j].style.height = arr[j] * 10 + "px"
-                bars[j].style.backgroundColor = "green"
-                bars[j+1].style.height = arr[j+1] * 10 + "px"  
-                bars[j+1].style.backgroundColor = "green"
-                await sleep(30)
+                bars[j].style.height = arr[j] * 15 + "px"
+                bars[j].style.backgroundColor = "red"
+                bars[j+1].style.height = arr[j+1] * 15 + "px"  
+                bars[j+1].style.backgroundColor = "red"
+                await sleep(1)
             }
+            bars[j].style.backgroundColor = "white"
+            bars[j+1].style.backgroundColor = "white" 
+            await sleep(1)
         }
-        await sleep(30)
+       
     }
     return array;
 }
