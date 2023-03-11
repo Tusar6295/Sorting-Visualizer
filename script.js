250f4d2519080056826303d958dbf12e7fe858a9
Tusar6295 <tusar_202000219@smit.smu.edu.in>
2023-03-11 21:20:17 +0530

    merge sort added

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const algo = document.getElementById('algo')
 
 let min = 1; 
 let max = 20;
-let array = [];
 let numOfBars = parseInt(sizeSlider.value);
+let array = new Array(numOfBars);
 let delay = parseInt(speedSlider.value);
 let algoUsed = "";
 
@@ -16,7 +16,7 @@ let algoUsed = "";
 newArray.addEventListener('click', function(){
     createArray();
     cont.textContent = ''
-    bars(array)
+    displayBars(array)
 })
 
 //slider to change array size
@@ -24,7 +24,7 @@ sizeSlider.addEventListener("input", function () {
     numOfBars = sizeSlider.value;
     cont.innerHTML = "";
     createArray();
-    bars(array);
+    displayBars(array);
   });
 
 //slider to change speed 
@@ -50,11 +50,11 @@ function createArray()
 //array is created and bars are rendered when dom is loaded
 document.addEventListener("DOMContentLoaded", function() {
     createArray()
-    bars(array)
+    displayBars(array)
 })
 
 //display bars based on array size
-function bars(array){
+function displayBars(array){
     for(let i=0; i<numOfBars; i++)
     {
         const bar = document.createElement('div')
@@ -70,11 +70,12 @@ function sleep(ms)
     return new Promise((resolve) => setTimeout(resolve,ms));
 }
 
+//const bars = document.getElementsByClassName('bar')
 //implement bubblesort algo
 async function bubbleSort(arr)
 {
     const bars = document.getElementsByClassName('bar')
-    for(let i = 0; i<numOfBars; i++)
+    for(let i = 0; i<arr.length; i++)
     {
         for(let j = 0; j<numOfBars - i - 1; j++)
         { 
@@ -103,7 +104,6 @@ async function bubbleSort(arr)
 async function insertionSort(arr)
 {
     const bars = document.getElementsByClassName('bar')
-
     for(let i = 1; i<numOfBars; i++)
     {
         let temp = arr[i];
@@ -164,6 +164,182 @@ async function selectionSort(arr)
     
 }
 
+//merge sort 
+
+async function merge(arr,s,e)
+{
+    const bars = document.getElementsByClassName('bar')
+    let mid =  s + parseInt((e-s)/2);
+
+    let len1 = mid - s + 1;
+    let len2 = e - mid;
+
+    let arr1 = new Array(len1)
+    let arr2 = new Array(len2)
+
+    let k = s;
+    for(let i=0; i<len1; i++)
+    {
+        arr1[i] = arr[k++];
+    }
+
+    for(let j=0; j<len2; j++)
+    {
+        arr2[j] = arr[k++]
+    }
+
+    k = s;
+    let i=0;
+    let j=0;
+
+    while(i < len1 && j < len2)
+    {
+        bars[k].style.backgroundColor = "yellow"
+        if(arr1[i] < arr2[j])
+        {
+            arr[k] = arr1[i];
+            bars[k].style.height = arr[k] * 15 + 'px';
+            
+            i++;
+            k++;
+        }
+        else{
+            arr[k] = arr2[j];
+            bars[k].style.height = arr[k] * 15 + 'px';
+            k++;
+            j++;
+        }
+        await sleep(delay)
+    }
+
+    while(i<len1)
+    {
+        bars[k].style.backgroundColor = "yellow"
+        arr[k] = arr1[i];
+        bars[k].style.height = arr[k] * 15 + 'px';
+        i++;
+        k++;
+        await sleep(delay)
+    }
+
+    while(j<len2)
+    {
+        bars[k].style.backgroundColor = "yellow"
+        arr[k] = arr2[j];
+        bars[k].style.height = arr[k] * 15 + 'px';
+        j++;
+        k++;
+        await sleep(delay)
+    }
+
+    for (let k = 0; k < bars.length; k++) {
+        bars[k].style.backgroundColor = "aqua";
+    }
+}
+
+async function mergeSort(arr,s,e)
+{
+    if(s>=e)
+    {
+        return;
+    }
+    
+    let mid =  s + parseInt((e-s)/2);
+
+
+    await mergeSort(arr,s,mid);
+    await mergeSort(arr,mid+1,e);
+
+    await merge(arr,s,e);
+}
+
+/*async function merge(arr, s, e) {
+    console.log("merge");
+    let bar = document.getElementsByClassName("bar");
+  
+    let mid = s + parseInt((e-s)/2)
+    let left = new Array(mid - s + 1);
+    let right = new Array(e - mid);
+  
+    // left.forEach((element, index) => {
+    //   element = arr[s + index];
+    // });
+  
+    // right.forEach((element, index) => {
+    //   element = arr[mid + 1 + index];
+    // });
+  
+    for (let i = 0; i < left.length; i++) {
+      left[i] = arr[s + i];
+    }
+    for (let i = 0; i < right.length; i++) {
+      right[i] = arr[mid + i + 1];
+    }
+  
+    let i = s;
+    let k = 0;
+    let j = 0;
+    while (j < left.length && k < right.length) {
+      bar[i].style.backgroundColor = "yellow";
+  
+      if (left[j] < right[k]) {
+        arr[i] = left[j];
+        bar[i].style.height = arr[i] * 15 + "px";
+        // bar[i].style.backgroundColor = "yellow";
+  
+        // console.log("while");
+  
+        j++;
+        i++;
+      } else {
+        arr[i] = right[k];
+        bar[i].style.height = arr[i] * 15 + "px";
+  
+        i++;
+        k++;
+      }
+  
+      await sleep(delay);
+    }
+  
+    while (j < left.length) {
+      bar[i].style.backgroundColor = "yellow";
+  
+      arr[i] = left[j];
+      bar[i].style.height = arr[i] * 15 + "px";
+  
+      j++;
+      i++;
+      await sleep(delay);
+    }
+    while (k < right.length) {
+      bar[i].style.backgroundColor = "yellow";
+  
+      arr[i] = right[k];
+      bar[i].style.height = arr[i] * 15 + "px";
+  
+      k++;
+      i++;
+      await sleep(delay);
+    }
+  
+
+    for (let k = 0; k < bar.length; k++) {
+      bar[k].style.backgroundColor = "aqua";
+    }
+  }
+
+  async function mergeSort(arr, s, e) {
+    if (s < e) {
+      // console.log(s + " ");
+      let mid = s + parseInt((e - s) / 2);
+      // let mid = parseInt((s + e) / 2);
+      await mergeSort(arr, s, mid);
+      await mergeSort(arr, mid + 1, e);
+  
+      await merge(arr, s, e, mid);
+    }
+  }*/
 sort.addEventListener('click' , function(){
     switch(algoUsed) {
         case("bubble") :
@@ -176,7 +352,7 @@ sort.addEventListener('click' , function(){
             selectionSort(array);
             break;
         case("merge"):
-            mergeSort(array);
+            mergeSort(array,0,numOfBars-1);
             break;
         case("quick"):
             quickSort(array);
